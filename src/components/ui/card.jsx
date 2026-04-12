0e9b2d27b5bc627070a16df05eb80719ec2901c3
YoSign <yosign@gmail.com>
2026-04-13 07:07:33 +0800

    refine: improve care item contrast, card spacing, and font rendering

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -13,7 +13,7 @@ Card.displayName = "Card"
 const CardHeader = React.forwardRef(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("flex flex-col space-y-1.5 p-6", className)}
+    className={cn("flex flex-col gap-2 p-5", className)}
     {...props} />
 ))
 CardHeader.displayName = "CardHeader"
@@ -35,7 +35,7 @@ const CardDescription = React.forwardRef(({ className, ...props }, ref) => (
 CardDescription.displayName = "CardDescription"
 
 const CardContent = React.forwardRef(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+  <div ref={ref} className={cn("px-5 pb-5 pt-0", className)} {...props} />
 ))
 CardContent.displayName = "CardContent"
 
